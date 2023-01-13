e939415e087f2b1c53efcc4a489bd66865cf1890
VitZelinka <zelinka.vit5@gmail.com>
2023-01-13 17:11:17 +0100

    added tests for AddJobToQ func

--- tests/planet_job_q.test.js
+++ tests/planet_job_q.test.js
@@ -19,6 +19,8 @@ function AddToQ(planet, type, info, finish) {
 
 console.log("DON'T FORGET TO COMMENT OUT DB CALL AT THE END OF THE FUNCTION !!!");
 
+// ---------------- RefreshPlanet() ---------------
+
 test("only default resUpdate", () => {
     AddToQ(planet, "resUpdate", 0, 0);
     func.RefreshPlanet(planet);
@@ -73,4 +75,54 @@ test("resUpdate -> mineUpgrade(metal,2.) -> resUpdate -> mineUpgrade(crystal,1.)
     expect(planet.mines.crystal).toEqual(1);
     expect(planet.resources.metal).toEqual(30*1 + 20*2);
     expect(planet.resources.crystal).toEqual(40);
+});
+
+// ---------------- AddJobToQ() ---------------------
+
+test("add mineUpgrade to default Q", () => {
+    AddToQ(planet, "resUpdate", 0, 0);
+    func.AddJobToQ(planet.jobQueue, {jobType: "mineUpgrade", jobInfo: "metal", finishAt: 120*1000});
+    expect(planet.jobQueue[0]).toEqual({jobType: "resUpdate", jobInfo: 120*1000, finishAt: 0});
+    expect(planet.jobQueue[1]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 120*1000});
+    expect(planet.jobQueue[2]).toEqual({jobType: "resUpdate", jobInfo: 0, finishAt: 120*1000+1});
+});
+
+test("add mineUpgrade before mineUpgrade", () => {
+    AddToQ(planet, "resUpdate", 150*1000, 0);
+    AddToQ(planet, "mineUpgrade", "metal", 150*1000);
+    AddToQ(planet, "resUpdate", 0, 150*1000+1);
+    func.AddJobToQ(planet.jobQueue, {jobType: "mineUpgrade", jobInfo: "metal", finishAt: 120*1000});
+    expect(planet.jobQueue[0]).toEqual({jobType: "resUpdate", jobInfo: 120*1000, finishAt: 0});
+    expect(planet.jobQueue[1]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 120*1000});
+    expect(planet.jobQueue[2]).toEqual({jobType: "resUpdate", jobInfo: 150*1000, finishAt: 120*1000+1});
+    expect(planet.jobQueue[3]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 150*1000});
+    expect(planet.jobQueue[4]).toEqual({jobType: "resUpdate", jobInfo: 0, finishAt: 150*1000+1});
+});
+
+test("add mineUpgrade after mineUpgrade", () => {
+    AddToQ(planet, "resUpdate", 120*1000, 0);
+    AddToQ(planet, "mineUpgrade", "metal", 120*1000);
+    AddToQ(planet, "resUpdate", 0, 120*1000+1);
+    func.AddJobToQ(planet.jobQueue, {jobType: "mineUpgrade", jobInfo: "metal", finishAt: 150*1000});
+    expect(planet.jobQueue[0]).toEqual({jobType: "resUpdate", jobInfo: 120*1000, finishAt: 0});
+    expect(planet.jobQueue[1]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 120*1000});
+    expect(planet.jobQueue[2]).toEqual({jobType: "resUpdate", jobInfo: 150*1000, finishAt: 120*1000+1});
+    expect(planet.jobQueue[3]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 150*1000});
+    expect(planet.jobQueue[4]).toEqual({jobType: "resUpdate", jobInfo: 0, finishAt: 150*1000+1});
+});
+
+test("add mineUpgrade between mineUpgrade's", () => {
+    AddToQ(planet, "resUpdate", 120*1000, 0);
+    AddToQ(planet, "mineUpgrade", "metal", 120*1000);
+    AddToQ(planet, "resUpdate", 140*1000, 120*1000+1);
+    AddToQ(planet, "mineUpgrade", "metal", 140*1000);
+    AddToQ(planet, "resUpdate", 0, 140*1000+1);
+    func.AddJobToQ(planet.jobQueue, {jobType: "mineUpgrade", jobInfo: "metal", finishAt: 130*1000});
+    expect(planet.jobQueue[0]).toEqual({jobType: "resUpdate", jobInfo: 120*1000, finishAt: 0});
+    expect(planet.jobQueue[1]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 120*1000});
+    expect(planet.jobQueue[2]).toEqual({jobType: "resUpdate", jobInfo: 130*1000, finishAt: 120*1000+1});
+    expect(planet.jobQueue[3]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 130*1000});
+    expect(planet.jobQueue[4]).toEqual({jobType: "resUpdate", jobInfo: 140*1000, finishAt: 130*1000+1});
+    expect(planet.jobQueue[5]).toEqual({jobType: "mineUpgrade", jobInfo: "metal", finishAt: 140*1000});
+    expect(planet.jobQueue[6]).toEqual({jobType: "resUpdate", jobInfo: 0, finishAt: 140*1000+1});
 });
