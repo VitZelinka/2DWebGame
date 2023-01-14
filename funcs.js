71edb12186f384525c1eed1848f3b34bb86a2c49
VitZelinka <zelinka.vit5@gmail.com>
2023-01-14 15:35:23 +0100

    rewrote RefreshPlanet to use ordered array

--- funcs.js
+++ funcs.js
@@ -34,74 +34,7 @@ exports.UpdatePlanetRes_CTime = function (planet, date) {
     planet.resUpdate = updateTime;
 }
 
-
 exports.RefreshPlanet = async function (planet) {
-    let dll = new LinkedList();
-    dll.insert("xd1");
-    dll.insert("xd2");
-    dll.insert("xd3");
-    let newQueue = [];
-    const currentTime = Date.now();
-    const loop_n = planet.jobQueue.length;
-    for (let i = 0; i < loop_n; i++) { // iterate thru every job
-        let min = planet.jobQueue[0].finishAt;
-        let minJob = planet.jobQueue[0];
-        let jobIndex = 0;
-        let tmpIndex = 0;
-        for (const job of planet.jobQueue) { // find smallest (time) job, store in minJob
-            if (job.finishAt < min) {min = job.finishAt; minJob = job; jobIndex = tmpIndex;}
-            tmpIndex++;
-        }
-        switch (minJob.jobType) { // finishedAt = when to calculate the job
-            case "resUpdate":
-                const jobFinishAt = minJob.finishAt;
-                if (jobFinishAt == 0) { // means do now
-                    if (minJob.jobInfo == 0) { // means do until now
-                        func.UpdatePlanetRes_CTime(planet, currentTime);
-                        newQueue.push(minJob);
-                    } else {
-                        if (minJob.jobInfo > currentTime) {
-                            func.UpdatePlanetRes_CTime(planet, currentTime);
-                            newQueue.push(minJob);
-                        } else {
-                            func.UpdatePlanetRes_CTime(planet, minJob.jobInfo);
-                            minJob.jobInfo = 0; // readds default job
-                            newQueue.push(minJob);
-                        }
-                    }
-                } else if (jobFinishAt > currentTime) {
-                    newQueue.push(minJob); // just skips
-                } else {
-                    if (minJob.jobInfo == 0) {
-                        func.UpdatePlanetRes_CTime(planet, currentTime);
-                    } else if (minJob.jobInfo > currentTime) {
-                        newQueue.push(minJob); // just skips
-                    } else {
-                        func.UpdatePlanetRes_CTime(planet, minJob.jobInfo);
-                    }
-                }
-                planet.jobQueue.splice(jobIndex, 1);
-                break;
-
-            case "mineUpgrade":
-                if (minJob.finishAt > currentTime) {
-                    newQueue.push(minJob);
-                    planet.jobQueue.splice(jobIndex, 1);
-                } else {
-                    planet.mines[minJob.jobInfo]++;
-                    planet.jobQueue.splice(jobIndex, 1);
-                }
-                break;
-
-            default:
-                break;
-        }
-    }
-    planet.jobQueue = newQueue;
-    //await planet.save();
-}
-
-exports.RefreshPlanet2 = async function (planet) {
     let newQueue = [];
     const currentTime = Date.now();
     const loop_n = planet.jobQueue.length;
@@ -111,6 +44,8 @@ exports.RefreshPlanet2 = async function (planet) {
 
         switch (job.jobType) { // finishedAt = when to calculate the job
             case "resUpdate":
+                if (job.finishAt > job.jobInfo && job.jobInfo !== 0){break;}
+                
                 if (job.finishAt == 0) { // means do now
                     if (job.jobInfo == 0) { // means do until now
                         func.UpdatePlanetRes_CTime(planet, currentTime);
@@ -152,7 +87,7 @@ exports.RefreshPlanet2 = async function (planet) {
     }
 
     planet.jobQueue = newQueue;
-    //await planet.save();
+    await planet.save();
 }
 
 
@@ -166,12 +101,12 @@ exports.AddJobToQ = function (planet, job) {
         case "mineUpgrade":
             let resUpdateJob = {jobType: "resUpdate", jobInfo: 0, finishAt: job.finishAt + 1};
             let biggerNode = q_dll.getHeadNode();
-            let wasntNull = true;
+            let wasNull = false;
             while (biggerNode.getData().finishAt <= job.finishAt) {
                 biggerNode = biggerNode.next;
                 if (biggerNode == null) {
                     biggerNode = q_dll.getTailNode();
-                    wasntNull = false;
+                    wasNull = true;
                     break;
                 }
             }
@@ -195,7 +130,7 @@ exports.AddJobToQ = function (planet, job) {
                 tmpNode = tmpNode.next;
             }
 
-            if (wasntNull) {
+            if (!wasNull) {
                 q_dll.insertBefore(biggerNode.getData(), job);
                 q_dll.insertBefore(biggerNode.getData(), resUpdateJob);
             } else {
